41ddee1494ee9cf0a62284efe8092676fa5110cc
Synappscommunity <synappscommunity@gmail.com>
2025-06-07 20:35:13 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Required for static export
   },
-  trailingSlash: true,
-  reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/synapps-website' : '',
 };
 
 export default nextConfig;
